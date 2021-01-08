404782423c1839a37391a74f174d4a6c78b9cd68
Tomasz Włodarczyk <tomasz.wlodarczyk@tueuropa.pl>
2021-01-08 14:57:10 +0100

    sceneria i samochód

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
+var http = require("http");
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname + '/main.css'));
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname + "/main.css"));
 });
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname + '/lib/three.js'));
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname + "/lib/three.js"));
 });
+app.get("/terrain", (req, res) => {
+  res.sendFile(path.join(__dirname + "/images/grasslight-big.jpg"));
+});
+app.get("/road", (req, res) => {
+  res.sendFile(path.join(__dirname + "/images/road.jpg"));
+});
+app.use("/static", express.static("models"));
+var httpServer = http.createServer(app);
 
-app.listen(8081);
+httpServer.listen(8081);
